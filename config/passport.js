0490c9c21a689debffd755f0205ce8c6160ffb7f
jinwoo2626 <jinwoojinho@naver.com>
2022-06-12 14:14:33 +0900

    주석 추가

--- config/passport.js
+++ config/passport.js
@@ -5,18 +5,18 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 module.exports = function (passport) {
-  passport.use(new LocalStrategy({usernameField: 'idemail'}, (idemail, password, done) => {
-    User.findOne({idemail: idemail}).then(user => {
-      if (!user) {
+  passport.use(new LocalStrategy({usernameField: 'idemail'}, (idemail, password, done) => {  //idemail로 회원찾기
+    User.findOne({idemail: idemail}).then(user => { 
+      if (!user) {    //회원이 없을 때
         console.log('User not found!');
         return done(null, false); // error, user, message
       }
 
-      bcrypt.compare(password, user.password, (err, isMatch) => {
+      bcrypt.compare(password, user.password, (err, isMatch) => {   //비밀번호 확인
         if (err) throw err;
-        if (isMatch) {
+        if (isMatch) {    //비밀번호 맞을 시
           return done(null, user);
-        } else {
+        } else {          //비밀번호 틀렸을 때
           console.log('Password incorrect!');
           return done(null, false);
         }
